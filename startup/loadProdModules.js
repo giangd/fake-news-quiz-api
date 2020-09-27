57a56f2b7f63d45217b74fc45f4d8dde4bc0f27f
Giang Duong <gtduong@ucsc.edu>
2020-09-26 23:04:01 -0700

    Fix CORS

--- startup/loadProdModules.js
+++ startup/loadProdModules.js
@@ -1,7 +1,9 @@
 const helmet = require("helmet");
 const compression = require("compression");
+const cors = require("cors");
 
 module.exports = (app) => {
+    app.use(cors({ credentials: true, origin: true }));
     app.use(helmet());
     app.use(compression());
 };
